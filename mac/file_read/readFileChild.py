021414df6ef57d4d91fd71edf4fa191969420fd5
kdt7058 <krishnatapse03@gmail.com>
2023-10-12 20:28:16 +0530

    update 1: read csv file

--- mac/file_read/readFileChild.py
+++ mac/file_read/readFileChild.py
@@ -1,11 +1,16 @@
+import pandas
+
+
 class ReadFileChild:
 
-    def __init__(self,file_obj):
+    def __init__(self, file_obj):
         self.read_conf = file_obj
 
     def read_file(self):
-
         fileFormat = self.read_conf.file_format
         fileName = self.read_conf.filename
-        fileLocation =self.read_conf.location
-        print("Output: ",fileLocation)
+        fileLocation = self.read_conf.location
+
+        full_path = "{}\{}.{}".format(fileLocation, fileName, fileFormat)
+        csvFile = pandas.read_csv(full_path, encoding='ISO-8859-1')
+        print(csvFile)
